3ab1c96594224c5948288e9f96e0a2d01f5eb6bc
mexicola84 <i@jan-grimm.com>
2023-11-30 16:33:29 +0100

    Build half of the logic in an if statement to recalculate the measurement units, but have to build the structur for the input type. After that, trying to get the logic a bit more streamlined.

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -8,43 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var lengthValue = 10
-    @State private var startMeasure = "m"
-    @State private var targetMeasure = "ft."
+    @State private var initValue = 10.0
+    @State private var startUnit = "meters"
+    @State private var targetUnit = "feet"
+    @FocusState private var lengthIsFocused: Bool
     
-    let measures = ["mm", "cm", "m", "km", "in.", "ft.", "yd.", "mi."]
+    let units = ["millimeters", "centimeters", "meters", "kilometers", "inches", "feet", "yards", "miles"]
     
-    var measureConvert: Int {
-        return 0
+    var unitConverter: Double {
+        let baseUnit = Measurement(value: initValue, unit: UnitLength.meters)
+        var convertedValue: Double = 0
+        
+        if targetUnit == startUnit {
+            convertedValue = initValue
+        } else if targetUnit == "millimeters" {
+            convertedValue = baseUnit.converted(to: UnitLength.millimeters).value
+        } else if targetUnit == "centimeters" {
+            convertedValue = baseUnit.converted(to: UnitLength.centimeters).value
+        } else if targetUnit == "meters" {
+            convertedValue = baseUnit.converted(to: UnitLength.meters).value
+        } else if targetUnit == "kilometers" {
+            convertedValue = baseUnit.converted(to: UnitLength.kilometers).value
+        } else if targetUnit == "inches" {
+            convertedValue = baseUnit.converted(to: UnitLength.inches).value
+        } else if targetUnit == "feet" {
+            convertedValue = baseUnit.converted(to: UnitLength.feet).value
+        } else if targetUnit == "yards" {
+            convertedValue = baseUnit.converted(to: UnitLength.yards).value
+        } else if targetUnit == "miles" {
+            convertedValue = baseUnit.converted(to: UnitLength.miles).value
+        }
+        
+        return Double(convertedValue)
     }
 
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    TextField("Input Measure", value: $lengthValue, format: .number)
-                    Picker("Input Format", selection: $startMeasure) {
-                        ForEach(measures, id: \.self) {
+                    TextField("Input Measure", value: $initValue, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($lengthIsFocused)
+                    Picker("Input Unit", selection: $startUnit) {
+                        ForEach(units, id: \.self) {
                             Text("\($0)")
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 
                 Section("Convert to:") {
-                    Picker("Target format", selection: $targetMeasure) {
-                        ForEach(measures, id: \.self) {
+                    Picker("Target Unit", selection: $targetUnit) {
+                        ForEach(units, id: \.self) {
                             Text("\($0)")
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 
                 Section("Final Value") {
-                    Text("\(measureConvert) \(targetMeasure)")
+                    Text("\(unitConverter) \(targetUnit)")
                 }
             }
             .navigationTitle("LengthConverter")
+            .toolbar {
+                if lengthIsFocused {
+                    Button("Done") {
+                        lengthIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
