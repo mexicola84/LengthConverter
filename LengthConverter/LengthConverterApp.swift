ec2c33e665f347c90c8809975a7fda37781df475
mexicola84 <i@jan-grimm.com>
2023-11-30 14:35:05 +0100

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Jan Grimm on 30.11.23.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
