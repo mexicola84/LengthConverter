8cd33d0f4c23ce9536ce881bec57d976ea388965
mexicola84 <i@jan-grimm.com>
2023-11-30 15:10:35 +0100

    Build the basic sections including pickers for my first project.

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var lengthValue = 10
+    @State private var startMeasure = "m"
+    @State private var targetMeasure = "ft."
+    
+    let measures = ["mm", "cm", "m", "km", "in.", "ft.", "yd.", "mi."]
+    
+    var measureConvert: Int {
+        return 0
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Input Measure", value: $lengthValue, format: .number)
+                    Picker("Input Format", selection: $startMeasure) {
+                        ForEach(measures, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Convert to:") {
+                    Picker("Target format", selection: $targetMeasure) {
+                        ForEach(measures, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Final Value") {
+                    Text("\(measureConvert) \(targetMeasure)")
+                }
+            }
+            .navigationTitle("LengthConverter")
         }
-        .padding()
     }
 }
 
